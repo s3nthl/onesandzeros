61a9d0d6dd401b23f5214cd9345d2aad116f11a2
s3nthl <43458813+s3nthl@users.noreply.github.com>
2020-04-13 22:54:18 -0400

    removed comments

--- main.tf
+++ main.tf
@@ -21,16 +21,6 @@ resource "azurerm_subnet" "testsubnet" {
   resource_group_name  = "${azurerm_resource_group.testdevopsrg.name}"
   virtual_network_name = "${azurerm_virtual_network.testvnet.name}"
   address_prefix       = "${var.testaddress_subnet}"
-  // network_security_group_id = "${azurerm_network_security_group.aks-nsg.id}"
-  // service_endpoints    = ["Microsoft.Sql"]
 }
-
-
-// resource "azurerm_resource_group" "corerg" {
-//   name     = "rg-gw-core-01"
-//   location = "UK South"
-// }
-
-
 
 
