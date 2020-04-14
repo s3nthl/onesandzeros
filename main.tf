911fc64a66066813d24bc2c380ab05f11bcf059c
senthil <s3nthl@gmail.com>
2020-04-13 22:58:40 -0400

    added commenrts

--- main.tf
+++ main.tf
@@ -14,6 +14,11 @@ resource "azurerm_virtual_network" "testvnet" {
   dns_servers         = "${var.dns_servers}"
   location            = "${azurerm_resource_group.testdevopsrg.location}"
   resource_group_name = "${azurerm_resource_group.testdevopsrg.name}"
+  //
+  //
+  //
+  //
+  //
 }
 
 resource "azurerm_subnet" "testsubnet" {
